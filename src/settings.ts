5a568dfe20b95ef024a6f47b21b5e84348dfa6fe
Trey Tomes <george.tomes@cityelectricsupply.com>
2023-01-22 14:05:01 -0600

    Increase tick interval from 1s to 10ms.

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,7 @@ export default {
 	'URL_KERNEL_WASM': 'kernel.wasm',
 	'TIM_IRQ': 0,
 	'KEY_IRQ': 1,
-	'TICK_INTERVAL_MS': 1000,
+	'TICK_INTERVAL_MS': 10,
 	'INITIAL_SIZE_PAGES': 256,
 	'MAXIMUM_SIZE_PAGES': 512
 };
